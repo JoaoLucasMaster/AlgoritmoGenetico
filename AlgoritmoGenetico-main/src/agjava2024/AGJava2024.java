a278a2424b617fb13bf550cf032afe1a4b5db584
João Lucas N.D <84422552+JoaoLucasMaster@users.noreply.github.com>
2024-07-18 21:39:42 -0300

    Update AGJava2024.java

--- AlgoritmoGenetico-main/src/agjava2024/AGJava2024.java
+++ AlgoritmoGenetico-main/src/agjava2024/AGJava2024.java
@@ -4,11 +4,11 @@ public class AGJava2024 {
 
     public static void main(String[] args) {
         int populacao = 20;
-        double capacidadePeso = 400;  // capacidade máxima de peso do avião
-        double larguraMaxima = 300;   // largura máxima da área de carga do avião
-        double alturaMaxima = 300;    // altura máxima da área de carga do avião
-        double profundidadeMaxima = 400;  // profundidade máxima da área de carga do avião
-        int probabilidadeMutacao = 5; // 5%
+        double capacidadePeso = 400;  
+        double larguraMaxima = 300; 
+        double alturaMaxima = 300;    
+        double profundidadeMaxima = 400;  
+        int probabilidadeMutacao = 5; 
         int qtdeCruzamentos = 5;
         int numeroGeracoes = 10;
 
